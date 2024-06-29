d9b90b59f7861ce6fb02df22adcf7ec39d574cf3
박종훈 <m05214@naver.com>
2024-06-29 16:33:20 +0900

    Feat: UsersRepository 추가
    UsersRepository 추가함

---
+++ wpms-api-server/src/main/java/webpagemanagementsystem/user/repository/UsersRepository.java
@@ -0,0 +1,10 @@
+package webpagemanagementsystem.user.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import webpagemanagementsystem.user.entity.Users;
+
+public interface UsersRepository extends JpaRepository<Users, Long> {
+
+    public List<Users> findByEmail(String email);
+}
