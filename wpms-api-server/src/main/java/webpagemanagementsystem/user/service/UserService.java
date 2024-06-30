26a7bcd439ef81b42b61f6dc20f6b7c5b45fde06
박종훈 <m05214@naver.com>
2024-06-30 17:33:27 +0900

    Feat: 사용할 메서드 추가함
    findByEmail과 convertUsersToFindByEmailResponse 메소드 정의

--- wpms-api-server/src/main/java/webpagemanagementsystem/user/service/UserService.java
+++ wpms-api-server/src/main/java/webpagemanagementsystem/user/service/UserService.java
@@ -1,4 +1,9 @@
 package webpagemanagementsystem.user.service;
 
-public class UserService {
+import webpagemanagementsystem.user.dto.FindByEmailResponse;
+import webpagemanagementsystem.user.entity.Users;
+
+public interface UserService {
+    public Users findByEmail(String email);
+    public FindByEmailResponse convertUsersToFindByEmailResponse(Users user);
 }
