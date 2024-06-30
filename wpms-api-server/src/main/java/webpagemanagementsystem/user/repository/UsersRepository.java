6ba27b980bb68f05e2259f8b1c397e60852d598b
박종훈 <m05214@naver.com>
2024-06-30 17:35:50 +0900

    Feat: UserRepository 메소드 추가
    UserRepository findByEmailAndIsUse 메서드 추가함

--- wpms-api-server/src/main/java/webpagemanagementsystem/user/repository/UsersRepository.java
+++ wpms-api-server/src/main/java/webpagemanagementsystem/user/repository/UsersRepository.java
@@ -2,9 +2,11 @@ package webpagemanagementsystem.user.repository;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import webpagemanagementsystem.common.entity.IsUseEnum;
 import webpagemanagementsystem.user.entity.Users;
 
 public interface UsersRepository extends JpaRepository<Users, Long> {
 
     public List<Users> findByEmail(String email);
+    public List<Users> findByEmailAndIsUse(String email, IsUseEnum isUse);
 }
