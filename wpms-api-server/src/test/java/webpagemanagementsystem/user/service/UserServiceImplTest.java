824da8efc96dfa1ca8478f555324ba8e8101bb76
박종훈 <m05214@naver.com>
2024-06-30 17:37:48 +0900

    Test: UserService에 대한 테스트 코드
    UserService 중 FindByEmail에 대한 검증 테스트코드 작성함

---
+++ wpms-api-server/src/test/java/webpagemanagementsystem/user/service/UserServiceImplTest.java
@@ -0,0 +1,68 @@
+package webpagemanagementsystem.user.service;
+
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import webpagemanagementsystem.common.entity.IsUseEnum;
+import webpagemanagementsystem.common.entity.YNEnum;
+import webpagemanagementsystem.user.entity.SocialType;
+import webpagemanagementsystem.user.entity.Users;
+import webpagemanagementsystem.user.repository.UsersRepository;
+
+import static org.mockito.BDDMockito.given;
+import static org.assertj.core.api.Assertions.*;
+
+
+@SpringBootTest
+class UserServiceImplTest {
+
+    @Autowired
+    private UserService userService;
+
+    @MockBean(name = "usersRepository")
+    private UsersRepository usersRepository;
+
+    @DisplayName("UsersService email로_Users_객체조회_실패")
+    @Test
+    public void test(){
+        // given
+        String email = "m11111@naver.com";
+
+        given(usersRepository.findByEmail(email)).willThrow(new NoSuchElementException());
+
+        // when & then
+        Assertions.assertThrows(NoSuchElementException.class, () -> {
+            userService.findByEmail(email);
+        });
+    }
+
+    @DisplayName("UsersService email로_Users_객체조회_성공")
+    @Test
+    public void test2(){
+        // given
+        String email = "m11111@naver.com";
+        Users user = Users.builder()
+                .name("박종훈")
+                .email(email)
+                .password(null)
+                .isSocial(YNEnum.U)
+                .socialId("18346737826")
+                .socialType(SocialType.KAKAO)
+                .picture(null)
+                .isUse(IsUseEnum.U)
+                .build();
+        given(usersRepository.findByEmail(email)).willReturn(Arrays.asList(user));
+
+        // when
+        Users resultUser = userService.findByEmail(email);
+
+        // then
+        assertThat(resultUser).isEqualTo(user);
+    }
+}
