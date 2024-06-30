6da1960f33731c4583a62af9b20cd48ac576ec8b
박종훈 <m05214@naver.com>
2024-06-30 17:34:21 +0900

    Feat: UserService 메서드 구현
    findByEmail과 convertUsersToFindByEmailResponse 구현

---
+++ wpms-api-server/src/main/java/webpagemanagementsystem/user/service/UserServiceImpl.java
@@ -0,0 +1,27 @@
+package webpagemanagementsystem.user.service;
+
+import java.util.NoSuchElementException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import webpagemanagementsystem.common.entity.IsUseEnum;
+import webpagemanagementsystem.user.dto.FindByEmailResponse;
+import webpagemanagementsystem.user.entity.Users;
+import webpagemanagementsystem.user.repository.UsersRepository;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+
+    private final UsersRepository usersRepository;
+
+    @Override
+    public Users findByEmail(String email) {
+        return usersRepository.findByEmailAndIsUse(email, IsUseEnum.U).stream().findFirst()
+                .orElseThrow(() -> new NoSuchElementException());
+    }
+
+    @Override
+    public FindByEmailResponse convertUsersToFindByEmailResponse(Users user) {
+        return new FindByEmailResponse(user);
+    }
+}
