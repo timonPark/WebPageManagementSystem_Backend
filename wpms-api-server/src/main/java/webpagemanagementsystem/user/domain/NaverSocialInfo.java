2a2af6dd151b504d376b9c8956bbaaa66d073fc9
박종훈 <m05214@naver.com>
2024-07-09 22:42:16 +0900

    Feat: 네이버 소셜로그인, 회원가입, 유효성 검사 구현
    네이버 소셜로그인, 회원가입, 유효성 검사
    
    Resolve: #wpms_back_15

--- wpms-api-server/src/main/java/webpagemanagementsystem/user/domain/NaverSocialInfo.java
+++ wpms-api-server/src/main/java/webpagemanagementsystem/user/domain/NaverSocialInfo.java
@@ -2,10 +2,29 @@ package webpagemanagementsystem.user.domain;
 
 import lombok.Data;
 import org.springframework.http.HttpStatusCode;
+import webpagemanagementsystem.common.entity.IsUseEnum;
+import webpagemanagementsystem.common.entity.YNEnum;
+import webpagemanagementsystem.user.entity.SocialType;
+import webpagemanagementsystem.user.entity.Users;
 
 @Data
 public class NaverSocialInfo {
-  HttpStatusCode status;
+  NaverResponse response;
+  String resultcode;
+  String message;
+
+  public Users convertNaverSocialInfoToUsers(){
+    return Users.builder()
+        .name(this.response.getName())
+        .email(this.response.getEmail())
+        .isSocial(YNEnum.Y)
+        .socialId(this.response.getId())
+        .socialType(SocialType.naver)
+        .picture(this.response.getProfileImage())
+        .isUse(IsUseEnum.U)
+        .build();
+
+  }
 
 
 }
--- wpms-api-server/src/main/java/webpagemanagementsystem/user/service/UserSocialServiceImpl.java
+++ wpms-api-server/src/main/java/webpagemanagementsystem/user/service/UserSocialServiceImpl.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -18,6 +19,9 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 import webpagemanagementsystem.common.jwt.AccessTokenProvider;
 import webpagemanagementsystem.user.entity.Users;
+import webpagemanagementsystem.user.exception.DeleteUserException;
+import webpagemanagementsystem.user.exception.DuplicationRegisterException;
+import webpagemanagementsystem.user.exception.NoUseException;
 import webpagemanagementsystem.user.exception.SocialUnauthorizedException;
 
 @Service
@@ -29,6 +33,8 @@ public class UserSocialServiceImpl {
   private final AuthenticationManagerBuilder authenticationManagerBuilder;
 
   private final ObjectMapper objectMapper;
+
+  private final UserService userService;
   public Map<String, Object> getSocialInfo(String provider, String accessToken, String baseSocaiUrl, String baseSocaiPathUrl)
       throws SocialUnauthorizedException {
     URI uri = UriComponentsBuilder
@@ -63,5 +69,29 @@ public class UserSocialServiceImpl {
     UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(user.getEmail(), user.getSocialId());
     Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
     return accessTokenProvider.createToken(authentication);
+  }
+
+  public String returnSocialLoginProgress(Users loginUser)
+      throws DuplicationRegisterException, DeleteUserException, NoUseException {
+    try {
+      Users registUser = userService.findByEmail(loginUser.getEmail())
+          .stream().findFirst()
+          .orElseThrow(NoSuchElementException::new);
+
+      // 중복 회원가입 방지
+      userService.preventDuplicationRegist(loginUser,registUser);
+
+      // 로그인 시 휴면처리
+      userService.validateUserIsUse(registUser);
+
+      // accessToken 발급
+      return socialAuthenticate(registUser);
+    } catch (NoSuchElementException e) {
+      // 회원가입
+      Users resultUser = userService.createUser(loginUser);
+
+      // accessToken 발급
+      return socialAuthenticate(resultUser);
+    }
   }
 }
