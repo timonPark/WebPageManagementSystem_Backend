6de44a5e218842d8e0b8574ba94cbb38d44f6d48
박종훈 <m05214@naver.com>
2024-07-15 22:32:04 +0900

    test: UserSocialServiceImpl 테스트코드 작성완료
    UserSocialServiceImpl 테스트코드 작성완료

--- wpms-api-server/src/test/java/webpagemanagementsystem/user/service/UserSocialServiceImplTest.java
+++ wpms-api-server/src/test/java/webpagemanagementsystem/user/service/UserSocialServiceImplTest.java
@@ -3,6 +3,7 @@ package webpagemanagementsystem.user.service;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.given;
 
+import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
@@ -10,7 +11,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+
 import webpagemanagementsystem.common.entity.IsUseEnum;
+import webpagemanagementsystem.common.entity.YNEnum;
+
 import webpagemanagementsystem.common.variable.SocialProperties;
 import webpagemanagementsystem.user.domain.GoogleSocialInfo;
 import webpagemanagementsystem.user.domain.KakaoAccount;
@@ -18,6 +22,11 @@ import webpagemanagementsystem.user.domain.KakaoProfile;
 import webpagemanagementsystem.user.domain.KakaoSocialInfo;
 import webpagemanagementsystem.user.domain.NaverResponse;
 import webpagemanagementsystem.user.domain.NaverSocialInfo;
+import webpagemanagementsystem.user.entity.SocialType;
+import webpagemanagementsystem.user.entity.Users;
+import webpagemanagementsystem.user.exception.DeleteUserException;
+import webpagemanagementsystem.user.exception.DuplicationRegisterException;
+import webpagemanagementsystem.user.exception.NoUseException;
 import webpagemanagementsystem.user.exception.SocialUnauthorizedException;
 import webpagemanagementsystem.user.repository.UsersRepository;
 
@@ -29,6 +38,9 @@ class UserSocialServiceImplTest {
 
   @Autowired
   private UserSocialServiceImpl userSocialServiceImpl;
+
+  @Autowired
+  private UserService userService;
 
   @MockBean(name = "usersRepository")
   private UsersRepository usersRepository;
@@ -147,41 +159,197 @@ class UserSocialServiceImplTest {
     assertThat(googleSocialInfo).isEqualTo(resultSocialInfo);
   }
 
-  @DisplayName("returnSocialLoginProgress 회원가입 후 AccessToken 발급")
+  @DisplayName("returnSocialLoginProgress DuplicationRegisterException 발생 - 중복회원 가입")
   @Test
-  void test5() throws SocialUnauthorizedException {
+  void test5() {
     // given
+    Users paramUser = Users.builder()
+        .name("박종훈")
+        .email("m05214@naver.com")
+        .isSocial(YNEnum.Y)
+        .socialId("cbhQt7JpqQkz87OVkH5EkPJZoMoMYKwYcyBGD9J2MC0")
+        .socialType(SocialType.naver)
+        .picture("https://phinf.pstatic.net/contact/20230306_83/1678064365001C3dar_JPEG/KakaoTalk_Photo_2019-09-11-15-24-01.jpeg")
+        .isUse(IsUseEnum.U)
+        .build();
 
-//    given(usersRepository.findByEmail(email)).willReturn(list);
-//    String accessToken = "ya29.a0AXooCgtrC6cYprCH5k9ct8MOnM25lhCZk4BskP5sXzJyf6_QpPeTPTmOV9t6ZXToscECN4g7xP4L4l19GtG96ME67EDT26V8YL0f8vzxE4L0JlsEcGCpPVocIf5vmDr0gwZtjAywCpEHWdgTlVNRl0GvGY7daALZ8_EaCgYKAfsSARMSFQHGX2MiB1lu_47fteUhBKi2Npn6Ag0170";
-//    String platformName = "google";
-//    Class<GoogleSocialInfo>  socialInfoType = GoogleSocialInfo.class;
-//    GoogleSocialInfo googleSocialInfo = new GoogleSocialInfo();
-//    googleSocialInfo.setId("112625927003722335487");
-//    googleSocialInfo.setEmail("kindcorder@gmail.com");
-//    googleSocialInfo.setVerifiedEmail("true");
-//    googleSocialInfo.setName("Park jonghoon");
-//    googleSocialInfo.setGivenName("Park");
-//    googleSocialInfo.setFamilyName("jonghoon");
-//    googleSocialInfo.setPicture("https://lh3.googleusercontent.com/a/ACg8ocIlMpTb4DRnXpwM2YhAqAVUS6D1QD5siteHn5IVIy9dPERcew=s96-c");
-//    googleSocialInfo.setLocale(null);
-//
-//    // when
-//    GoogleSocialInfo resultSocialInfo =userSocialServiceImpl.getSocialInfo(
-//        platformName,
-//        accessToken,
-//        socialProperties.platform.get(platformName).getBaseUrl(),
-//        socialProperties.platform.get(platformName).getPathUrl(),
-//        socialInfoType
-//    );
-//
-//    // then
-//    assertThat(googleSocialInfo).isEqualTo(resultSocialInfo);
+    String email = "m05214@naver.com";
+    List<Users> list = List.of(
+        Users.builder()
+            .name("박종훈")
+            .email("m05214@naver.com")
+            .isSocial(YNEnum.N)
+            .socialId("cbhQt7JpqQkz87OVkH5EkPJZoMoMYKwYcyBGD9J2MC0")
+            .socialType(SocialType.naver)
+            .picture("https://phinf.pstatic.net/contact/20230306_83/1678064365001C3dar_JPEG/KakaoTalk_Photo_2019-09-11-15-24-01.jpeg")
+            .isUse(IsUseEnum.U)
+            .build()
+    );
+    given(usersRepository.findByEmail(email)).willReturn(list);
+
+
+    // when & than
+    Assertions.assertThrows(DuplicationRegisterException.class, () -> {
+      userSocialServiceImpl.returnSocialLoginProgress(paramUser);
+    });
+  }
+
+  @DisplayName("returnSocialLoginProgress DuplicationRegisterException 발생 - 중복회원 가입2")
+  @Test
+  void test6(){
+    // given
+    Users paramUser = Users.builder()
+        .name("박종훈")
+        .email("m05214@naver.com")
+        .isSocial(YNEnum.Y)
+        .socialId("cbhQt7JpqQkz87OVkH5EkPJZoMoMYKwYcyBGD9J2MC0")
+        .socialType(SocialType.naver)
+        .picture("https://phinf.pstatic.net/contact/20230306_83/1678064365001C3dar_JPEG/KakaoTalk_Photo_2019-09-11-15-24-01.jpeg")
+        .isUse(IsUseEnum.U)
+        .build();
+
+    String email = "m05214@naver.com";
+    List<Users> list = List.of(
+        Users.builder()
+            .name("박종훈")
+            .email("m05214@naver.com")
+            .isSocial(YNEnum.Y)
+            .socialId("cbhQt7JpqQkz87OVkH5EkPJZoMoMYKwYcyBGD9J2MC0")
+            .socialType(SocialType.kakao)
+            .picture("https://phinf.pstatic.net/contact/20230306_83/1678064365001C3dar_JPEG/KakaoTalk_Photo_2019-09-11-15-24-01.jpeg")
+            .isUse(IsUseEnum.U)
+            .build()
+    );
+    given(usersRepository.findByEmail(email)).willReturn(list);
+
+
+    // when & than
+    Assertions.assertThrows(DuplicationRegisterException.class, () -> {
+      userSocialServiceImpl.returnSocialLoginProgress(paramUser);
+    });
+  }
+
+  @DisplayName("returnSocialLoginProgress NoUseException 발생 - 휴면계정")
+  @Test
+  void test8(){
+    // given
+    Users paramUser = Users.builder()
+        .name("박종훈")
+        .email("m05214@naver.com")
+        .isSocial(YNEnum.Y)
+        .socialId("cbhQt7JpqQkz87OVkH5EkPJZoMoMYKwYcyBGD9J2MC0")
+        .socialType(SocialType.naver)
+        .picture("https://phinf.pstatic.net/contact/20230306_83/1678064365001C3dar_JPEG/KakaoTalk_Photo_2019-09-11-15-24-01.jpeg")
+        .isUse(IsUseEnum.N)
+        .build();
+
+    String email = "m05214@naver.com";
+    List<Users> list = List.of(
+        Users.builder()
+            .name("박종훈")
+            .email("m05214@naver.com")
+            .isSocial(YNEnum.Y)
+            .socialId("cbhQt7JpqQkz87OVkH5EkPJZoMoMYKwYcyBGD9J2MC0")
+            .socialType(SocialType.naver)
+            .picture("https://phinf.pstatic.net/contact/20230306_83/1678064365001C3dar_JPEG/KakaoTalk_Photo_2019-09-11-15-24-01.jpeg")
+            .isUse(IsUseEnum.N)
+            .build()
+    );
+    given(usersRepository.findByEmail(email)).willReturn(list);
+
+
+    // when & than
+    Assertions.assertThrows(NoUseException.class, () -> {
+      userSocialServiceImpl.returnSocialLoginProgress(paramUser);
+    });
+  }
+
+  @DisplayName("returnSocialLoginProgress DeleteUserException 발생 - 삭제계정")
+  @Test
+  void test9(){
+    // given
+    Users paramUser = Users.builder()
+        .name("박종훈")
+        .email("m05214@naver.com")
+        .isSocial(YNEnum.Y)
+        .socialId("cbhQt7JpqQkz87OVkH5EkPJZoMoMYKwYcyBGD9J2MC0")
+        .socialType(SocialType.naver)
+        .picture("https://phinf.pstatic.net/contact/20230306_83/1678064365001C3dar_JPEG/KakaoTalk_Photo_2019-09-11-15-24-01.jpeg")
+        .isUse(IsUseEnum.D)
+        .build();
+
+    String email = "m05214@naver.com";
+    List<Users> list = List.of(
+        Users.builder()
+            .name("박종훈")
+            .email("m05214@naver.com")
+            .isSocial(YNEnum.Y)
+            .socialId("cbhQt7JpqQkz87OVkH5EkPJZoMoMYKwYcyBGD9J2MC0")
+            .socialType(SocialType.naver)
+            .picture("https://phinf.pstatic.net/contact/20230306_83/1678064365001C3dar_JPEG/KakaoTalk_Photo_2019-09-11-15-24-01.jpeg")
+            .isUse(IsUseEnum.D)
+            .build()
+    );
+    given(usersRepository.findByEmail(email)).willReturn(list);
+
+
+    // when & than
+    Assertions.assertThrows(DeleteUserException.class, () -> {
+      userSocialServiceImpl.returnSocialLoginProgress(paramUser);
+    });
+  }
+
+  @DisplayName("returnSocialLoginProgress accessToken 발급")
+  @Test
+  void test10() throws DuplicationRegisterException, DeleteUserException, NoUseException {
+    // given
+    Users paramUser = Users.builder()
+        .name("박종훈")
+        .email("m05214@naver.com")
+        .isSocial(YNEnum.Y)
+        .socialId("cbhQt7JpqQkz87OVkH5EkPJZoMoMYKwYcyBGD9J2MC0")
+        .socialType(SocialType.naver)
+        .picture("https://phinf.pstatic.net/contact/20230306_83/1678064365001C3dar_JPEG/KakaoTalk_Photo_2019-09-11-15-24-01.jpeg")
+        .isUse(IsUseEnum.D)
+        .build();
+    Users createUser = Users.builder()
+        .userNo(15L)
+        .name("박종훈")
+        .email("m05214@naver.com")
+        .isSocial(YNEnum.Y)
+        .socialId("cbhQt7JpqQkz87OVkH5EkPJZoMoMYKwYcyBGD9J2MC0")
+        .socialType(SocialType.naver)
+        .picture("https://phinf.pstatic.net/contact/20230306_83/1678064365001C3dar_JPEG/KakaoTalk_Photo_2019-09-11-15-24-01.jpeg")
+        .isUse(IsUseEnum.D)
+        .build();
+
+    String email = "m05214@naver.com";
+    List<Users> findByEmailAndIsUse = List.of(Users.builder()
+        .userNo(15L)
+        .name("박종훈")
+        .email("m05214@naver.com")
+        .isSocial(YNEnum.Y)
+        .socialId("cbhQt7JpqQkz87OVkH5EkPJZoMoMYKwYcyBGD9J2MC0")
+        .socialType(SocialType.naver)
+        .picture("https://phinf.pstatic.net/contact/20230306_83/1678064365001C3dar_JPEG/KakaoTalk_Photo_2019-09-11-15-24-01.jpeg")
+        .isUse(IsUseEnum.D)
+        .build());
+    List<Users> list = List.of();
+    given(usersRepository.findByEmail(email)).willReturn(list);
+    given(usersRepository.save(paramUser)).willReturn(createUser);
+    given(usersRepository.findByEmailAndIsUse(email, IsUseEnum.U)).willReturn(findByEmailAndIsUse);
+
+
+    // when
+    String accessToken = userSocialServiceImpl.returnSocialLoginProgress(paramUser);
+
+    assertThat(accessToken).isInstanceOf(String.class);
+
   }
 
   @DisplayName("returnSocialLoginProgress 로그인 후 AccessToken 발급")
   @Test
-  void test6() throws SocialUnauthorizedException {
+  void test7() throws SocialUnauthorizedException {
     // given
 
 //    given(usersRepository.findByEmail(email)).willReturn(list);
