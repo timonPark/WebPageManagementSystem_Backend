e83c6ddc02207f57cd51e738d66a9db504eac19b
박종훈 <m05214@naver.com>
2024-08-07 23:42:27 +0900

    Feature: Controller, Service findAll 구현
    Controller, Service findAll 구현

--- wpms-api-server/src/main/java/webpagemanagementsystem/menu/service/MenuServiceImple.java
+++ wpms-api-server/src/main/java/webpagemanagementsystem/menu/service/MenuServiceImple.java
@@ -1,7 +1,10 @@
 package webpagemanagementsystem.menu.service;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import webpagemanagementsystem.menu.dto.MenuResDto;
+import webpagemanagementsystem.menu.entity.Menu;
 import webpagemanagementsystem.menu.repository.MenuRepository;
 
 @Service
@@ -9,4 +12,21 @@ import webpagemanagementsystem.menu.repository.MenuRepository;
 public class MenuServiceImple implements MenuService{
 
   private final MenuRepository menuRepository;
+
+  @Override
+  public List<MenuResDto> findAll() {
+    return convertMenuToMenuResDto(menuRepository.findAll());
+  }
+
+  public List<MenuResDto> convertMenuToMenuResDto(List<Menu> menus) {
+    return menus.stream().map(
+                        menu -> MenuResDto.builder()
+                              .menuNo(menu.getMenuNo())
+                              .name(menu.getName())
+                              .url(menu.getUrl())
+                              .children(menu.getChildren())
+                              .orderNo(menu.getOrderNo())
+                              .build()
+                        ).toList();
+  }
 }
