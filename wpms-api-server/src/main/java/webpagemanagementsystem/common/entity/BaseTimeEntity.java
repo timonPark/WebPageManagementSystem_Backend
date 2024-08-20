1ebacb4bd26b93b1dcfd120ed41a97261ecb91f9
박종훈 <m05214@naver.com>
2024-08-20 22:29:34 +0900

    test: Menu Test 코드 작성 및 기능구현-1

--- wpms-api-server/src/main/java/webpagemanagementsystem/common/entity/BaseTimeEntity.java
+++ wpms-api-server/src/main/java/webpagemanagementsystem/common/entity/BaseTimeEntity.java
@@ -4,6 +4,7 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -11,6 +12,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @MappedSuperclass
+@Setter
 @EntityListeners(AuditingEntityListener.class)
 public class BaseTimeEntity {
 
--- wpms-api-server/src/test/java/webpagemanagementsystem/menu/service/MenuServiceImpleTest.java
+++ wpms-api-server/src/test/java/webpagemanagementsystem/menu/service/MenuServiceImpleTest.java
@@ -1,21 +1,71 @@
 package webpagemanagementsystem.menu.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
 
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import webpagemanagementsystem.common.entity.IsUseEnum;
+import webpagemanagementsystem.common.entity.YNEnum;
+import webpagemanagementsystem.menu.entity.Menu;
+import webpagemanagementsystem.menu.repository.MenuRepository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 
 @SpringBootTest
 class MenuServiceImpleTest {
+
+  @MockBean(name="menuRepository")
+  private MenuRepository menuRepository;
 
   @DisplayName("menuList 조회 - 성공")
   @Test
   void test(){
     // given
+    Menu menu = Menu.builder()
+        .menuNo(1L)
+        .name("Home")
+        .url("")
+        .orderNo(1)
+        .isManager(YNEnum.N)
+        .build();
+
+    menu.setIsUse(IsUseEnum.U);
+    menu.setCreatedNo(1L);
+    menu.setUpdatedNo(1L);
+    menu.setCreatedAt(LocalDateTime.now());
+    menu.setUpdatedAt(LocalDateTime.now());
+
+    Menu menu2 = Menu.builder()
+        .menuNo(2L)
+        .name("Page")
+        .url("")
+        .orderNo(2)
+        .isManager(YNEnum.Y)
+        .build();
+
+    menu.setIsUse(IsUseEnum.U);
+    menu.setCreatedNo(1L);
+    menu.setUpdatedNo(1L);
+    menu.setCreatedAt(LocalDateTime.now());
+    menu.setUpdatedAt(LocalDateTime.now());
+
+    List<Menu> menus = Arrays.asList(menu, menu2);
+
+    given(menuRepository.findAll()).willReturn(menus);
 
     // when & then
-    assertThat("").isNull();
+    List<Menu> result = menuRepository.findAll();
+
+    assertThat(result).isEqualTo(menus);
 
   }
 
