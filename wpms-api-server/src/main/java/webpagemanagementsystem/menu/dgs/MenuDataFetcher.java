feafd5cde62713fe809fab50deee61d1587bc735
박종훈 <m05214@naver.com>
2024-08-21 23:05:35 +0900

    feature/wpms_back_38 Menu 포멧 생성 및 뮤테이션 정리중

--- wpms-api-server/src/main/java/webpagemanagementsystem/menu/dgs/MenuDataFetcher.java
+++ wpms-api-server/src/main/java/webpagemanagementsystem/menu/dgs/MenuDataFetcher.java
@@ -1,9 +1,12 @@
 package webpagemanagementsystem.menu.dgs;
 
 import com.netflix.graphql.dgs.DgsComponent;
+import com.netflix.graphql.dgs.DgsMutation;
 import com.netflix.graphql.dgs.DgsQuery;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import webpagemanagementsystem.common.graphql.ApiResponseFormat;
 import webpagemanagementsystem.menu.entity.Menu;
 import webpagemanagementsystem.menu.service.MenuService;
 
@@ -13,8 +16,17 @@ public class MenuDataFetcher {
   private final MenuService menuService;
 
   @DgsQuery
-  public List<Menu> menus(){
-    return this.menuService.findAll();
+  public ApiResponseFormat<List<Menu>> menus(){
+    try {
+      return ApiResponseFormat.success(HttpStatus.OK, this.menuService.findAll());
+    } catch (Exception e) {
+      return ApiResponseFormat.error(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+    }
+  }
+
+  @DgsMutation
+  public ApiResponseFormat<String> createMenus(){
+    return null;
   }
 
 }
